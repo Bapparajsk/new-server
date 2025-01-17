6b03bd8482c2ec16842f77f79b4da667bcdf2550
Bapparaj sk <bapparajsk97@gmail.com>
2025-01-17 11:05:22 +0530

    feat: Implement job queue with BullMQ and S3
    Adds BullMQ for background job processing,
    including producer and worker setup. Also,
    introduces AWS S3 integration for file
    handling and presigned URLs.

---
+++ src/config/bullmq.config.ts
@@ -0,0 +1,12 @@
+import { Queue, QueueOptions } from 'bullmq';
+
+const QueueConfig: QueueOptions = {
+    connection: {
+        port: Number.parseInt(process.env.REDIS_PORT || "6379"), // Redis port
+        host: process.env.REDIS_HOST || "127.0.0.1", // Redis host
+    }
+}
+
+// export const newChatNotificationQueue = new Queue("newChatNotificationQueue", QueueConfig)
+// export const newPostNotificationQueue = new Queue("newPostNotificationQueue", QueueConfig)
+// export const friendNotificationQueue = new Queue("friendNotificationQueue", QueueConfig)
