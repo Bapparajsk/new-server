ab9959b6811cf2c85dc3fa4feb660322c7729fe8
Bapparaj sk <bapparajsk97@gmail.com>
2025-01-28 13:56:22 +0530

    chore: Remove Firebase config
    Firebase messaging is not currently used, so remove
    its configuration to clean up code and avoid potential
    errors.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 import transporter from './nodemailer.config';
 import { passport, authenticateAndRedirect } from "./passport.config";
-import {messaging} from './firebase.config';
+// import {messaging} from './firebase.config';
 import AWS_S3 from "./aws.config";
 import redisConfig from "./redis.config";
 
-export { transporter, passport, authenticateAndRedirect, messaging, AWS_S3, redisConfig };
+export { transporter, passport, authenticateAndRedirect, AWS_S3, redisConfig };
