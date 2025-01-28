ca4a39b6a1adddfcfe56e3c63b730524b4710a5b
Bapparaj sk <bapparajsk97@gmail.com>
2025-01-28 14:17:23 +0530

    fix: set secure cookie and sameSite to none
    Sets the `secure` option to `true` and `sameSite` to `none` for the `authToken` cookie.
    This ensures the cookie is only sent over HTTPS and allows cross-site access.

--- src/lib/setCookie.ts
+++ src/lib/setCookie.ts
@@ -3,8 +3,8 @@ import { Response } from 'express';
 export const setCookie= (res: Response, token: string) => {
     res.cookie('authToken', token, {
         httpOnly: true,
-        secure: false,
-        sameSite: 'lax',
+        secure: true,
+        sameSite: 'none',
         maxAge: 1000 * 60 * 60 * 24 * 2,
     });
 }
